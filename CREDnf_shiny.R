c161a36bf664093413705528cf64997be01ea7fe
Hugo <hugo.pedder@bristol.ac.uk>
2019-10-21 20:26:01 +0100

    Added checklist summary using ordered lists

--- CREDnf_shiny.R
+++ CREDnf_shiny.R
@@ -50,7 +50,30 @@ if (interactive()) {
       tabPanel("Outcome measures - Brain"),
       tabPanel("Outcome measures - Behaviour"),
       tabPanel("Data storage"),
-      tabPanel("Checklist summary"),
+      tabPanel("Checklist summary",
+               h3("CRED-nf checklist summary output"),
+               tags$div(
+                 tags$ol(
+                   tags$li("Pre-experiment"),
+                   tags$ol(
+                     tags$li(textOutput("summary1a")),
+                     tags$li(textOutput("summary1b")),
+                     type="a"
+                   ),
+
+                   tags$li("Control groups"),
+                   tags$ol(
+                     tags$li(textOutput("summary2a")),
+                     tags$li(textOutput("summary2b")),
+                     tags$li(textOutput("summary2c")),
+                     type="a"
+                   )
+                 )
+               )
+               # textOutput("summary1a"),
+               # br(),
+               # textOutput("summary1b")
+               ),
       widths=c(2,10)
     )
 
@@ -146,10 +169,64 @@ if (interactive()) {
     output$text2c <- renderText({
       if (input$checklist2c == "Yes") {
         return(input$response2c)
-      } else if (input$checklist2c == "No") {
-        return("This experiment did not use blinding")
       }
     })
+
+
+
+    output$summary1a <- renderText({
+      if (input$checklist1a=="Yes") {
+        return(input$response1a)
+      } else if (input$checklist1a=="No") {
+        return("Protocol and planned analyses were not pre-registered")
+      } else if (input$checklist1a=="Not applicable") {
+        return("Protocol and planned analyses were not applicable")
+      }
+    })
+
+    output$summary1b <- renderText({
+      if (input$checklist1b=="Yes") {
+        return(input$response1b)
+      } else if (input$checklist1b=="No") {
+        return("No justification was given for the sample size")
+      } else if (input$checklist1b=="Not applicable") {
+        return("A sample size was not applicable for this study")
+      }
+    })
+
+
+    output$summary2a <- renderText({
+      if (input$checklist2a=="Yes") {
+        return(input$response2a)
+      } else if (input$checklist2a=="No") {
+        return("No control group(s)/condition(s) were employed in this study")
+      } else if (input$checklist2a=="Not applicable") {
+        return("Control group(s)/condition(s) were not applicable for this study")
+      }
+    })
+
+    output$summary2b <- renderText({
+      if (input$checklist2b=="Yes") {
+        return(input$response2b)
+      } else if (input$checklist2b=="No") {
+        return("This study did not use a double-blind design")
+      } else if (input$checklist2b=="Not applicable") {
+        return("A double-blind design was not applicable for this study")
+      }
+    })
+
+    output$summary2c <- renderText({
+      if (input$checklist2c=="Yes") {
+        return(input$response2c)
+      } else if (input$checklist2c=="No") {
+        return("Those who rated the outcomes or those who performed the analysis were not blinded")
+      } else if (input$checklist2c=="Not applicable") {
+        return("Blinding of those who rated the outcomes or those who performed the analysis was not applicable for this study")
+      }
+    })
+
+    # Add option to export to PDF and/or Docx
+    # Add warning (and specify number) of any responses marked as "NO" at start of summary before output
 
   }
 
