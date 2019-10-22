6ad5068644093c0e5cf4ed20cdbdb418430543f1
Hugo <hugo.pedder@bristol.ac.uk>
2019-10-22 17:13:25 +0100

    Added domains 4 and 5

--- CREDnf_shiny.R
+++ CREDnf_shiny.R
@@ -54,8 +54,9 @@ if (interactive()) {
                              choices = list("No" = 1, "Yes" = 2, "Not applicable"=3), selected = NULL),
                  uiOutput("new2e"),
                  textOutput("text2e")
+               )
                ),
-               ),
+
       tabPanel("Control measures",
                h3("Control measures"),
                wellPanel(
@@ -71,22 +72,80 @@ if (interactive()) {
                  textOutput("text3b")
                ),
                wellPanel(
-                 textInput("response3c", label="Report the strategies participants used", placeholder="Copy text from manuscript that meets checklist criteria"),
+                 selectInput("checklist3c", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new3c"),
                  textOutput("text3c")
                ),
                wellPanel(
-                 textInput("response3d", label="Report methods used for online-data processing and artifact correction", placeholder="Copy text from manuscript that meets checklist criteria"),
+                 selectInput("checklist3d", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new3d"),
                  textOutput("text3d")
                ),
                wellPanel(
-                 textInput("response3e", label="Report condition and group effects for artifacts", placeholder="Copy text from manuscript that meets checklist criteria"),
+                 selectInput("checklist3e", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new3e"),
                  textOutput("text3e")
                )
       ),
 
-      tabPanel("Control measures"),
-      tabPanel("Feedback specifications"),
-      tabPanel("Outcome measures - Brain"),
+      tabPanel("Feedback specifications",
+               h3("Feedback specifications"),
+               wellPanel(
+                 selectInput("checklist4a", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new4a"),
+                 textOutput("text4a")
+               ),
+               wellPanel(
+                 selectInput("checklist4b", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new4b"),
+                 textOutput("text4b")
+               ),
+               wellPanel(
+                 selectInput("checklist4c", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new4c"),
+                 textOutput("text4c")
+               ),
+               wellPanel(
+                 selectInput("checklist4d", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new4d"),
+                 textOutput("text4d")
+               ),
+               wellPanel(
+                 selectInput("checklist4e", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new4e"),
+                 textOutput("text4e")
+               )
+               ),
+
+      tabPanel("Outcome measures - Brain",
+               h3("Outcome measures - Brain"),
+               wellPanel(
+                 selectInput("checklist5a", h4(),
+                             choices = list("Not applicable" = 1, "Provide details" = 2), selected = NULL),
+                 uiOutput("new5a"),
+                 textOutput("text5a")
+               ),
+               wellPanel(
+                 selectInput("checklist5b", h4(),
+                             choices = list("No" = 1, "Yes" = 2, "Not applicable"=3), selected = NULL),
+                 uiOutput("new5b"),
+                 textOutput("text5b")
+               ),
+               wellPanel(
+                 selectInput("checklist5c", h4(),
+                             choices = list("No" = 1, "Yes" = 2, "Not applicable"=3), selected = NULL),
+                 uiOutput("new5c"),
+                 textOutput("text5c")
+               )
+               ),
       tabPanel("Outcome measures - Behaviour"),
       tabPanel("Data storage"),
       tabPanel("Checklist summary",
@@ -118,12 +177,27 @@ if (interactive()) {
                      tags$li(textOutput("summary3d")),
                      tags$li(textOutput("summary3e")),
                      type="a"
+                   ),
+
+                   tags$li("Feedback specifications"),
+                   tags$ol(
+                     tags$li(textOutput("summary4a")),
+                     tags$li(textOutput("summary4b")),
+                     tags$li(textOutput("summary4c")),
+                     tags$li(textOutput("summary4d")),
+                     tags$li(textOutput("summary4e")),
+                     type="a"
+                   ),
+
+                   tags$li("Outcome measures - Brain"),
+                   tags$ol(
+                     tags$li(textOutput("summary5a")),
+                     tags$li(textOutput("summary5b")),
+                     tags$li(textOutput("summary5c")),
+                     type="a"
                    )
                  )
                )
-               # textOutput("summary1a"),
-               # br(),
-               # textOutput("summary1b")
                ),
       widths=c(2,10)
     )
@@ -138,23 +212,24 @@ if (interactive()) {
 
     # A vector of pre-existing choices:
     mychoices <- c("No", "Yes", "Not applicable")
+    mychoices2 <- c("Not applicable", "Provide details")
 
     observe({
-      updateSelectInput(session, inputId = "checklist1a", label = "Pre-register experiment protocol and planned analysis",
+      updateSelectInput(session, inputId = "checklist1a", label = "Does the study have a pre-registered protocol including planned analyses?",
                         choices = mychoices)
     })
     observe({
-      updateSelectInput(session, inputId = "checklist1b", label = "Justify sample size",
+      updateSelectInput(session, inputId = "checklist1b", label = "Is there a justification for the sample size used?",
                         choices = mychoices)
     })
 
 
     observe({
-      updateSelectInput(session, inputId = "checklist2a", label = "Employ control group(s) or control condition(s)",
+      updateSelectInput(session, inputId = "checklist2a", label = "Were control group(s) or control condition(s) employed?",
                         choices = mychoices)
     })
     observe({
-      updateSelectInput(session, inputId = "checklist2b", label = "When leveraging experimental designs where a double-blind is possible, use a double-blind",
+      updateSelectInput(session, inputId = "checklist2b", label = "When leveraging experimental designs where a double-blind is possible, was a double-blind design used?",
                         choices = mychoices)
     })
     observe({
@@ -177,6 +252,54 @@ if (interactive()) {
     })
     observe({
       updateSelectInput(session, inputId = "checklist3b", label = "Were participants provided with a strategy?",
+                        choices = mychoices)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist3c", label = "Report the strategies participants used",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist3d", label = "Report methods used for online data-processing and artifact collection",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist3e", label = "Report condition and group effects for artifacts",
+                        choices = mychoices2)
+    })
+
+
+    observe({
+      updateSelectInput(session, inputId = "checklist4a", label = "Report how the online-feature extraction was defined",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist4b", label = "Report and justify the reinforcement schedule",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist4c", label = "Report the feedback modality and content",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist4d", label = "Collect and report all brain activity variable(s) and/or contasts used for feedback, as displayed to experimental participants",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist4e", label = "Report the hardware and software used",
+                        choices = mychoices2)
+    })
+
+
+    observe({
+      updateSelectInput(session, inputId = "checklist5a", label = "Report neurofeedback regulation success based on the feedback signal",
+                        choices = mychoices2)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist5b", label = "Have within-session and between-session regulation blocks of feedback variable(s) been plotted, as well as pre-to-post resting baselines or contrasts?",
+                        choices = mychoices)
+    })
+    observe({
+      updateSelectInput(session, inputId = "checklist5c", label = "Has the experimental condition/group been statistically compared to the control condition/group (not only each group to baseline variables)?",
                         choices = mychoices)
     })
 
@@ -232,6 +355,68 @@ if (interactive()) {
         textInput("response3b", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
       }
     })
+    output$new3c <- renderUI({
+      if (!input$checklist3c == "Provide details") return(NULL) else {
+        textInput("response3c", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new3d <- renderUI({
+      if (!input$checklist3d == "Provide details") return(NULL) else {
+        textInput("response3d", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new3e <- renderUI({
+      if (!input$checklist3e == "Provide details") return(NULL) else {
+        textInput("response3e", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+
+
+    output$new4a <- renderUI({
+      if (!input$checklist4a == "Provide details") return(NULL) else {
+        textInput("response4a", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new4b <- renderUI({
+      if (!input$checklist4b == "Provide details") return(NULL) else {
+        textInput("response4b", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new4c <- renderUI({
+      if (!input$checklist4c == "Provide details") return(NULL) else {
+        textInput("response4c", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new4d <- renderUI({
+      if (!input$checklist4d == "Provide details") return(NULL) else {
+        textInput("response4d", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new4e <- renderUI({
+      if (!input$checklist4e == "Provide details") return(NULL) else {
+        textInput("response4e", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+
+
+    output$new5a <- renderUI({
+      if (!input$checklist5a == "Provide details") return(NULL) else {
+        textInput("response5a", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+    output$new5b <- renderUI({
+      if (!input$checklist5b == "Yes") return(NULL) else {
+        textInput("response5b", label=NULL, placeholder="Report page/figure number(s) in which plots are shown")
+      }
+    })
+    output$new5c <- renderUI({
+      if (!input$checklist5c == "Yes") return(NULL) else {
+        textInput("response5c", label=NULL, placeholder="Copy text from manuscript that meets checklist criteria")
+      }
+    })
+
+
+
 
 
     # Return text
@@ -240,7 +425,6 @@ if (interactive()) {
         return(input$response1a)
       }
     })
-
     output$text1b <- renderText({
       if (input$checklist1b == "Yes") {
         return(input$response1b)
@@ -285,8 +469,70 @@ if (interactive()) {
         return(input$response3b)
       }
     })
+    output$text3c <- renderText({
+      if (input$checklist3c == "Provide details") {
+        return(input$response3c)
+      }
+    })
+    output$text3d <- renderText({
+      if (input$checklist3d == "Provide details") {
+        return(input$response3d)
+      }
+    })
+    output$text3e <- renderText({
+      if (input$checklist3e == "Provide details") {
+        return(input$response3e)
+      }
+    })
 
 
+    output$text4a <- renderText({
+      if (input$checklist4a == "Provide details") {
+        return(input$response4a)
+      }
+    })
+    output$text4b <- renderText({
+      if (input$checklist4b == "Provide details") {
+        return(input$response4b)
+      }
+    })
+    output$text4c <- renderText({
+      if (input$checklist4c == "Provide details") {
+        return(input$response4c)
+      }
+    })
+    output$text4d <- renderText({
+      if (input$checklist4d == "Provide details") {
+        return(input$response4d)
+      }
+    })
+    output$text4e <- renderText({
+      if (input$checklist4e == "Provide details") {
+        return(input$response4e)
+      }
+    })
+
+
+    output$text5a <- renderText({
+      if (input$checklist5a == "Provide details") {
+        return(input$response4a)
+      }
+    })
+    output$text5b <- renderText({
+      if (input$checklist5b == "Yes") {
+        return(input$response5b)
+      }
+    })
+    output$text5c <- renderText({
+      if (input$checklist5c == "Yes") {
+        return(input$response5c)
+      }
+    })
+
+
+
+
+    ############# Report summary #############
 
     output$summary1a <- renderText({
       if (input$checklist1a=="Yes") {
@@ -381,16 +627,97 @@ if (interactive()) {
     })
 
     output$summary3c <- renderText({
-      return(input$response3c)
+      if (input$checklist3c=="Provide details") {
+        return(input$response3c)
+      } else if (input$checklist3c=="Not applicable") {
+        return("Participant strategies were not applicable for this study")
+      }
     })
 
     output$summary3d <- renderText({
-      return(input$response3d)
+      if (input$checklist3d=="Provide details") {
+        return(input$response3d)
+      } else if (input$checklist3d=="Not applicable") {
+        return("Online data-processing and artifact collection was not applicable for this study")
+      }
     })
 
     output$summary3e <- renderText({
-      return(input$response3e)
+      if (input$checklist3e=="Provide details") {
+        return(input$response3e)
+      } else if (input$checklist3e=="Not applicable") {
+        return("Condition and group effects for artifacts were not applicable for this study")
+      }
     })
+
+
+    output$summary4a <- renderText({
+      if (input$checklist4a=="Provide details") {
+        return(input$response4a)
+      } else if (input$checklist4a=="Not applicable") {
+        return("Online-feature extraction was not applicable for this study")
+      }
+    })
+
+    output$summary4b <- renderText({
+      if (input$checklist4b=="Provide details") {
+        return(input$response4b)
+      } else if (input$checklist4b=="Not applicable") {
+        return("A reinforcement schedule was not applicable for this study")
+      }
+    })
+
+    output$summary4c <- renderText({
+      if (input$checklist4c=="Provide details") {
+        return(input$response4c)
+      } else if (input$checklist4c=="Not applicable") {
+        return("Feedback modality and content was not applicable for this study")
+      }
+    })
+
+    output$summary4d <- renderText({
+      if (input$checklist4d=="Provide details") {
+        return(input$response4d)
+      } else if (input$checklist4d=="Not applicable") {
+        return("Brain activity variable(s) and/or contrasts used for feedback were not applicable for this study")
+      }
+    })
+
+    output$summary4e <- renderText({
+      if (input$checklist4e=="Provide details") {
+        return(input$response4e)
+      } else if (input$checklist4e=="Not applicable") {
+        return("No hardware of software were used in this study")
+      }
+    })
+
+
+    output$summary5a <- renderText({
+      if (input$checklist5a=="Provide details") {
+        return(input$response5a)
+      } else if (input$checklist5a=="Not applicable") {
+        return("Neurofeedback regulation success based on the feedback signal was not applicable for this study")
+      }
+    })
+    output$summary5b <- renderText({
+      if (input$checklist5b=="Yes") {
+        return(input$response5b)
+      } else if (input$checklist5b=="No") {
+        return("Within-session and between-session regulation blocks of feedback variable(s) were not plotted in this study")
+      } else if (input$checklist5b=="Not applicable") {
+        return("Plots of within-session and between-session regulation blocks of feedback variable(s) were not applicable for this study")
+      }
+    })
+    output$summary5c <- renderText({
+      if (input$checklist5c=="Yes") {
+        return(input$response5c)
+      } else if (input$checklist5c=="No") {
+        return("The experimental condition/group and the control condition(s)/group(s) were not statistically compared")
+      } else if (input$checklist5c=="Not applicable") {
+        return("Statistical comparison of the experimental condition/group and the control condition(s)/group(s) was not applicable for this study")
+      }
+    })
+
 
     # Add option to export to PDF and/or Docx
     # Add warning (and specify number) of any responses marked as "NO" at start of summary before output
