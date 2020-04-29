4883e88e0e8df56f83b7d620417ba5f766b0851e
Hugo <hugo.pedder@bristol.ac.uk>
2020-04-29 15:48:32 +0100

    Added tests using shinytest

---
+++ CREDnf/tests/RunTests.R
@@ -0,0 +1,5 @@
+# Run all tests
+library(shinytest)
+
+source("CREDnf/tests/Domain1-3.R")
+source("CREDnf/tests/Domain4-7.R")
---
+++ CREDnf_tests.R
@@ -0,0 +1,5 @@
+# CRED-nf online tool - Tests
+
+source("CREDnf/app.R")
+
+shinyApp(ui, server)
